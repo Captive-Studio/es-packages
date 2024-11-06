de903abcd3562f8274e1b9f1446e593b5e1dffb5
Julien Polo <julien.polo@captive.fr>
2024-11-06 10:40:42 +0100

    ♻️ Add CSSVariables

--- packages/cherry-sea/src/type/CSS.ts
+++ packages/cherry-sea/src/type/CSS.ts
@@ -1,3 +1,14 @@
 export type CSSColor = string;
 
+/**
+ * A CSS size (width or height or padding, etc).
+ * It can be in px, em, rem, ...
+ */
 export type CSSSize = string;
+
+/**
+ * A CSSVariables dictionary
+ */
+export interface CSSVariables {
+  [key: string]: string;
+}
