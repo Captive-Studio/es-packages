cda5c8c87fa4e92bded17e22cdd17f3a6025a0b5
Julien Polo <julien.polo@captive.fr>
2024-11-08 18:23:25 +0100

    ♻️ Remove non spec colors
    Reason : color definition will be discussed with the team

--- packages/cherry-sea/src/type/Color.ts
+++ packages/cherry-sea/src/type/Color.ts
@@ -1,33 +1,12 @@
 import type { CSSColor } from './CSS.js';
 
 export interface Color {
-  // Primary
   primary: CSSColor;
   onPrimary: CSSColor;
-  primaryContainer: CSSColor;
-  onPrimaryContainer: CSSColor;
-  // Secondary
-  secondary: CSSColor;
-  onSecondary: CSSColor;
-  secondaryContainer: CSSColor;
-  onSecondaryContainer: CSSColor;
-  // Tertiary
-  tertiary: CSSColor;
-  onTertiary: CSSColor;
-  tertiaryContainer: CSSColor;
-  onTertiaryContainer: CSSColor;
-  // Error
-  error: CSSColor;
-  onError: CSSColor;
-  errorContainer: CSSColor;
-  onErrorContainer: CSSColor;
 
   surface: CSSColor;
   onSurface: CSSColor;
 
-  outlineVariant: CSSColor;
   onSurfaceVariant: CSSColor;
-
-  outline: CSSColor;
   white: CSSColor;
 }
